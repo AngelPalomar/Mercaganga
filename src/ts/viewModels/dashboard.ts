7305fdd4e4788d1a400a692f1d1dae11968a9fdd
Angel Palomar <angelpg.123@hotmail.com>
2022-09-06 11:16:16 -0500

    Añadido manejo de gráficas

--- src/ts/viewModels/dashboard.ts
+++ src/ts/viewModels/dashboard.ts
@@ -1,37 +1,32 @@
 import * as AccUtils from "../accUtils";
+import * as ko from 'knockout';
+import ArrayDataProvider = require('ojs/ojarraydataprovider');
+
+//datos
+import * as datosJson from 'text!../dummy/bar.json';
+
+import 'ojs/ojknockout';
+import 'ojs/ojchart';
+import 'ojs/ojtoolbar';
+import "ojs/ojswitch";
+import "ojs/ojselectcombobox";
+
 class DashboardViewModel {
 
+  //Creamos nuestras variables
+  datosProvider: ArrayDataProvider<string, null>;
+  stackValue: ko.Observable<string>;
+  isVertical: ko.Observable<boolean>;
+  graficaTipo: ko.Observable<string>;
+
   constructor() {
-
-  }
-
-  /**
-   * Optional ViewModel method invoked after the View is inserted into the
-   * document DOM.  The application can put logic that requires the DOM being
-   * attached here.
-   * This method might be called multiple times - after the View is created
-   * and inserted into the DOM and after the View is reconnected
-   * after being disconnected.
-   */
-  connected(): void {
-    AccUtils.announce("Dashboard page loaded.");
-    document.title = "Dashboard";
-    // implement further logic if needed
-  }
-
-  /**
-   * Optional ViewModel method invoked after the View is disconnected from the DOM.
-   */
-  disconnected(): void {
-    // implement if needed
-  }
-
-  /**
-   * Optional ViewModel method invoked after transition to the new View is complete.
-   * That includes any possible animation between the old and the new View.
-   */
-  transitionCompleted(): void {
-    // implement if needed
+    //Inicializar datos
+    this.stackValue = ko.observable("off");
+    this.datosProvider = new ArrayDataProvider(JSON.parse(datosJson), {
+      keyAttributes: 'id'
+    });
+    this.isVertical = ko.observable(false);
+    this.graficaTipo = ko.observable("bar");
   }
 }
 
